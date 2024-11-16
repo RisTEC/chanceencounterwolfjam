7db20973596c8715122df6b40b4df9ec176b7008
Rishit Roshan <92199045+RisTEC@users.noreply.github.com>
2024-11-16 17:10:12 -0500

    perks

--- WolfJamChanceEncounter/Assets/Scripts/Perks.cs
+++ WolfJamChanceEncounter/Assets/Scripts/Perks.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Perks : MonoBehaviour
 {
+    public enum type
+    {
+        damage,
+        heal,
+        stamina
+    }
+    public type enemyType;
+    private float modifier;
+    public string message;
+    private List<Perks> allPerks = new List<Perks>();
+    [SerializeField]
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void CreatePerk(type type, string message)
+    {
+        enemyType = type;
+        this.message = message;
+        switch (type)
+        {
+            case type.damage:
+                modifier = Random.Range(10,21);
+                break;
+            case type.heal:
+                modifier = Random.Range(10, 21);
+                break;
+            case type.stamina:
+                modifier = Random.Range(10, 11);
+                break;
+        }
+
+        allPerks.Add(this);
     }
 }
